1501211391280495059cdf2ec717318dc2613c75
Maxi Moncada <maxilucianomoncada@gmail.com>
2026-05-19 19:15:51 -0300

    Clase Credito y Cuenta rellenada, cambios en el diagrama

--- src/models/Credito.java
+++ src/models/Credito.java
@@ -1,5 +1,105 @@
 package src.models;
+import java.time.LocalDate;
 
-public class Credito {
+/**
+ * Esta clase representa una tarjeta de credito en el sistema.
+ * 
+ * Gestiona el acceso a tarjetas de credito segun el tipo de cliente:
+ * - Clientes Plata: Sin acceso a tarjeta
+ * - Clientes Oro: Tarjeta Credix con limite de 250000
+ * - Clientes Platino: Tarjeta Premium con limite de 500000
+ * 
+ * @author Maxi Moncada, Matias Moyano, Maximo Zalazar, Darull Farid
+ * @version 3.0
+ * @see Cuenta
+ */
+public class Credito extends Cuenta {
     
+    private String tipoTarjeta;    
+    private int limiteCredito;
+
+    /**
+     * 
+     * @param tipoTarjeta es el nombre de la tarjeta
+     * @param numeroCuenta es el identificador de la cuenta
+     * @param fechaApertura es la fecha de apertura
+     * @param limiteCredito es el limite de credito
+     */
+    public Credito(String tipoTarjeta, String numeroCuenta, LocalDate fechaApertura, int limiteCredito) {
+        super(numeroCuenta, fechaApertura);
+        this.tipoTarjeta = tipoTarjeta;
+        this.limiteCredito = limiteCredito;
+    }
+
+    /**
+     * Obtiene el tipo de tarjeta
+     * 
+     * @return el tipo de tarjeta
+     */
+    public String getTipoTarjeta() {
+        return tipoTarjeta;
+    }
+
+    /**
+     * Establece el tipo de tarjeta
+     * 
+     * @param tipoTarjeta es el nuevo tipo de tarjeta
+     */
+    public void setTipoTarjeta(String tipoTarjeta) {
+        this.tipoTarjeta = tipoTarjeta;
+    }
+
+    /**
+     * Obtiene el limite de credito de la tarjeta
+     * 
+     * @return el limite de credito
+     */
+    public int getLimiteCredito() {
+        return limiteCredito;
+    }
+
+    /**
+     * Establece el limite de credito de la tarjeta
+     * 
+     * @param limiteCredito es el nuevo limite de credito
+     */
+    public void setLimiteCredito(int limiteCredito) {
+        this.limiteCredito = limiteCredito;
+    }
+
+    /**
+     * Se crea una tarjeta de credito segun el tipo de cliente
+     * 
+     * @param tipoCliente es el tipo de cliente (Plata, Oro, Platino)
+     * @param numeroCuenta es el numero de la cuenta
+     * @param fechaApertura es la fecha de apertura
+     * @return una instancia de Credito configurada segun el tipo, o null si no tiene acceso
+     * @throws IllegalArgumentException si el tipo de cliente no es valido
+     */
+    public static Credito crearTarjetaSegunCliente(String tipoCliente, String numeroCuenta, LocalDate fechaApertura) {
+        if (tipoCliente == null || tipoCliente.isEmpty()) {
+            throw new IllegalArgumentException("El tipo de cliente no puede estar vacio");
+        }
+
+        switch (tipoCliente) {
+            case "Plata":
+                return null; // Sin acceso a tarjeta de crédito
+            case "Oro":
+                return new Credito("Credix", numeroCuenta, fechaApertura, 250000);
+            case "Platino":
+                return new Credito("Premium", numeroCuenta, fechaApertura, 500000);
+            default:
+                throw new IllegalArgumentException("Tipo de cliente no valido: " + tipoCliente);
+        }
+    }
+
+    /**
+     * Valida si un cliente tiene acceso a tarjeta de credito segun su tipo.
+     * 
+     * @param tipoCliente el tipo de cliente
+     * @return true si el cliente tiene acceso, false en caso contrario
+     */
+    public static boolean tieneAccesoTarjeta(String tipoCliente) {
+        return !"Plata".equals(tipoCliente);
+    }
 }
--- src/models/Cuenta.java
+++ src/models/Cuenta.java
@@ -2,11 +2,11 @@ package src.models;
 import java.time.LocalDate;
 
 /**
- * Representa una cuenta bancaria genérica en el sistema.
+ * Representa una cuenta bancaria comun
  * 
- * Esta es la clase padre para (Caja de Ahorro, Crédito).
- * Almacena la información básica de una cuenta: su identificador único y la fecha
- * en que fue abierta.
+ * Esta es la clase padre para (Caja de Ahorro, Credito).
+ * Almacena la informacion basica de una cuenta: su identificador unico y la fecha
+ * en que fue abierta
  * 
  * @author Maxi Moncada, Matias Moyano, Maximo Zalazar, Farid Darull
  * @version 2.0
@@ -16,17 +16,17 @@ import java.time.LocalDate;
 
 public class Cuenta {
 
-    /** Identificador único de la cuenta. Formato flexible para adaptarse a diferentes sistemas. */
+    /** Es el identificador unico de la cuenta*/
     private String numeroCuenta;
     
-    /** Fecha en la cual la cuenta fue abierta en el banco. */
+    /** La fecha en la cual la cuenta fue abierta en el banco*/
     private LocalDate fechaApertura;
 
     /**
-     * Construye una nueva Cuenta con el número y fecha de apertura especificados.
+     * Construye una nueva Cuenta con el numero y fecha de apertura especificados
      * 
-     * @param numeroCuenta el identificador único de la cuenta
-     * @param fechaApertura la fecha en que se abrió la cuenta
+     * @param numeroCuenta es el identificador unico de la cuenta
+     * @param fechaApertura es la fecha en que se abrio la cuenta
      */
     public Cuenta(String numeroCuenta, LocalDate fechaApertura) {
         this.numeroCuenta = numeroCuenta;
@@ -34,16 +34,16 @@ public class Cuenta {
     }
 
     /**
-     * Obtiene el número (identificador) de la cuenta.
+     * Obtiene el numero (identificador) de la cuenta
      * 
-     * @return el número de cuenta
+     * @return el numero de cuenta
      */
     public String getNumeroCuenta() {
         return numeroCuenta;
     }
 
     /**
-     * Obtiene la fecha de apertura de la cuenta.
+     * Obtiene la fecha de apertura de la cuenta
      * 
      * @return la fecha de apertura
      */
@@ -52,23 +52,21 @@ public class Cuenta {
     }
 
     /**
-     * Establece el número de cuenta.
+     * Establece el numero de cuenta.
      * 
-     * @param numeroCuenta el nuevo número de cuenta
+     * @param numeroCuenta es el nuevo numero de cuenta
      */
     public void setNumeroCuenta(String numeroCuenta) {
         this.numeroCuenta = numeroCuenta;
     }
 
     /**
-     * Establece la fecha de apertura de la cuenta.
+     * Establece la fecha de apertura de la cuenta
      * 
-     * @param fechaApertura la nueva fecha de apertura
+     * @param fechaApertura es la nueva fecha de apertura
      */
     public void setFechaApertura(LocalDate fechaApertura) {
         this.fechaApertura = fechaApertura;
     }
 
-
-    
 }
