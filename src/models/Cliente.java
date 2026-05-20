b1f5c5faba22f524eff2f38524995a54985ee62c
Matias Moyano Bustos <matiasmoyanobustos@gmail.com>
2026-05-19 21:39:02 -0300

    Agregar implementaciones de Persona, Empleado y Cliente en main

--- src/models/Cliente.java
+++ src/models/Cliente.java
@@ -1,5 +1,21 @@
-package src.models;
+package models;
+/**
+ * Representa a un cliente dentro del sistema.
+ * Hereda los atributos y métodos de la clase Persona.
+ *
+ * @author Matías, Maxi, Máximo, Farid
+ * @version 1.0
+ * @see Persona
+ */
+public class Cliente extends Persona {
 
-public class Cliente {
-    
+    private String tipoCliente;
+
+    public Cliente(String dni, String nombre, int añoIngreso, boolean estado, String tipoCliente) {
+        super(dni, nombre, añoIngreso, estado);
+        this.tipoCliente = tipoCliente;
+    }
+
+    public String getTipoCliente(){return tipoCliente;}
+    public void setTipoCliente(String tipoCliente){this.tipoCliente = tipoCliente;}
 }
